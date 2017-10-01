4e7ba8260296489c740d260db2ad8250c4fa54d8
Benjamin Peters <bpeters@coli.uni-saarland.de>
2017-10-01 12:48:45 +0200

    improve documentation

--- onmt/modules/OutputLayer.py
+++ onmt/modules/OutputLayer.py
@@ -1,20 +1,21 @@
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class OutputLayer(nn.Module):
     """
-    Basically a wrapper around the nn.Sequential generator
+    A basic output layer combining a weight matrix and a log softmax.
+    The functionality here could be implemented with nn.Sequential;
+    however, it is kept separate as this code may be extended to allow
+    for other sorts of output layers.
     """
-    def __init__(self, rnn_size, vocab_size, share_decoder_embeddings=False):
+    def __init__(self, rnn_size, vocab_size):
         super(OutputLayer, self).__init__()
-        self.softmax_layer = nn.Sequential(
-            nn.Linear(rnn_size, vocab_size),
-            nn.LogSoftmax()
-        )
-        '''
-        if share_decoder_embeddings:
-            self.softmax_layer[0].weight = decoder.embeddings.word_lut.weight
-        '''
+        self.output_weights = nn.Linear(rnn_size, vocab_size)
 
     def forward(self, layer):
-        return self.softmax_layer(layer)
+        """
+        layer: (tgt_len * batch) x rnn_size
+        returns: (tgt_len * batch) x vocab_size
+        """
+        return F.log_softmax(self.output_weights(layer))
