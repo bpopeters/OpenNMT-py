f560536243cc63056215f234c867c27fe4f7994f
bpopeters <bpeters@coli.uni-saarland.de>
2018-08-03 16:29:44 +0100

    remove Trainer private methods that used the report manager

--- onmt/trainer.py
+++ onmt/trainer.py
@@ -42,7 +42,6 @@ def build_trainer(opt, model, fields, optim, data_type, model_saver=None):
     gpu_verbose_level = opt.gpu_verbose_level
     report_every = opt.report_every
 
-    # report_manager = onmt.utils.build_report_manager(opt)
     trainer = onmt.Trainer(model, train_loss, valid_loss, optim, trunc_size,
                            shard_size, data_type, norm_method,
                            grad_accum_count, n_gpu, gpu_rank,
@@ -69,8 +68,6 @@ class Trainer(object):
             data_type(string): type of the source input: [text|img|audio]
             norm_method(string): normalization methods: [sents|tokens]
             grad_accum_count(int): accumulate gradients this many times.
-            report_manager(:obj:`onmt.utils.ReportMgrBase`):
-                the object that creates reports, or None
             model_saver(:obj:`onmt.models.ModelSaverBase`): the saver is
                 used to save a checkpoint.
                 Thus nothing will be saved if this parameter is None
@@ -80,7 +77,6 @@ class Trainer(object):
                  trunc_size=0, shard_size=32, data_type='text',
                  norm_method="sents", grad_accum_count=1, n_gpu=1, gpu_rank=1,
                  gpu_verbose_level=0, report_every=50, model_saver=None):
-        # Basic attributes.
         self.model = model
         self.train_loss = train_loss
         self.valid_loss = valid_loss
@@ -133,7 +129,7 @@ class Trainer(object):
 
         total_stats = onmt.utils.Statistics()
         report_stats = onmt.utils.Statistics()
-        # self._start_report_manager(start_time=total_stats.start_time)
+
         start_time = total_stats.start_time
 
         while self.current_step <= train_steps:
@@ -176,11 +172,8 @@ class Trainer(object):
                         report_stats.output(
                             self.current_step, train_steps,
                             self.learning_rate, start_time)
-                        # you may additionally do tensorboard writing here
+                        # you additionally do tensorboard writing here
 
-                    # hidden in the weeds is the fact that afterwards
-                    # report_stats is ALWAYS an empty Statistics object
-                    # self._maybe_report_training(train_steps, report_stats)
                     report_stats = onmt.utils.Statistics()
 
                     if self.current_step % valid_steps == 0:
@@ -200,7 +193,7 @@ class Trainer(object):
                                     % valid_stats.ppl())
                         logger.info('Validation accuracy: %g'
                                     % valid_stats.accuracy())
-                        # you may additionally do tensorboard writing here
+                        # you additionally do tensorboard writing here
 
                     if self.gpu_rank == 0:
                         self._maybe_save()
@@ -294,16 +287,6 @@ class Trainer(object):
             onmt.utils.distributed.all_reduce_and_rescale_tensors(
                 grads, float(1))
 
-    def _start_report_manager(self, start_time=None):
-        """
-        Simple function to start report manager (if any)
-        """
-        if self.report_manager is not None:
-            if start_time is None:
-                self.report_manager.start()
-            else:
-                self.report_manager.start_time = start_time
-
     def _maybe_gather_stats(self, stat):
         """
         Gather statistics in multi-processes cases
@@ -318,26 +301,6 @@ class Trainer(object):
         if stat is not None and self.n_gpu > 1:
             return onmt.utils.Statistics.all_gather_stats(stat)
         return stat
-
-    def _maybe_report_training(self, num_steps, report_stats):
-        """
-        Simple function to report training stats (if report_manager is set)
-        see `onmt.utils.ReportManagerBase.report_training` for doc
-        """
-        if self.report_manager is not None:
-            return self.report_manager.report_training(
-                self.current_step, num_steps, self.learning_rate, report_stats,
-                multigpu=self.n_gpu > 1)
-
-    def _report_step(self, train_stats=None, valid_stats=None):
-        """
-        Simple function to report stats (if report_manager is set)
-        see `onmt.utils.ReportManagerBase.report_step` for doc
-        """
-        if self.report_manager is not None:
-            return self.report_manager.report_step(
-                self.learning_rate, self.current_step, train_stats=train_stats,
-                valid_stats=valid_stats)
 
     def _maybe_save(self):
         """
