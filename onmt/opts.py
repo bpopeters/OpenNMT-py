54924d97603d244c302ba206a9dbebeb891f36de
bpopeters <bpeters@coli.uni-saarland.de>
2018-11-23 14:59:12 +0000

    add teacher forcing ratio

--- onmt/opts.py
+++ onmt/opts.py
@@ -392,6 +392,9 @@ def train_opts(parser):
                        Set to zero to turn off label smoothing.
                        For more detailed information, see:
                        https://arxiv.org/abs/1512.00567""")
+    group.add_argument('-teacher_forcing_ratio', type=float, default=1.0,
+                       help="""Probability that teacher forcing will be applied
+                       to a batch.""")
     # learning rate
     group = parser.add_argument_group('Optimization- Rate')
     group.add_argument('-learning_rate', type=float, default=1.0,
